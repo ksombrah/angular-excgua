69530f63d1094dcf2118330f3143e65bed1dd917
ksombrah <sombra@alcionesytes.net>
2021-05-10 10:51:41 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-excgua
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-excgua)
---
+++ src/app/products.ts
@@ -0,0 +1,27 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Desenvolvimento WEB',
+    price: 40,
+    description: 'Preço por hora trabalhada'
+  },
+  {
+    id: 2,
+    name: 'Assistência Técnica',
+    price: 70,
+    description: 'Preço da Visita Técnica com solução do problema'
+  },
+  {
+    id: 3,
+    name: 'Declaração de IRPF',
+    price: 70,
+    description: 'Valor simples para regularizar o CPF'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
